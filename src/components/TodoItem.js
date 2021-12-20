84e47d72988853286f52a4b62b6e8a0c821ef047
Mohit <mynkchauhan1234@gmail.com>
2021-12-20 19:24:46 +0530

    Created

---
+++ src/components/TodoItem.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './TodoItem.css'
+import Checkbox from '@material-ui/core/Checkbox';
+ import { useDispatch } from 'react-redux';
+ import { setCheck } from '../features/todoSlice';
+const TodoItem = ({name, done, id}) => {
+    const dispatch = useDispatch()
+    const handleCheck = () => {
+       dispatch(setCheck(id))
+    }
+    return (
+        <div className = 'todoItem'>
+            <Checkbox
+                checked = {done}
+                color = 'primary'
+                onChange = {handleCheck}
+                inputProps = {{'arial-label' : 'secondary checkbox'}}
+                />
+            <p classname= {done && 'todoItem--done'}>{name}</p>   
+        </div>
+    )
+}
+
+export default TodoItem
